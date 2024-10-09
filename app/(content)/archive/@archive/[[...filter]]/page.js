c36427ded8983f7217bd8b58154ac726ebc5aae7
shariq <shariqsiddiqui593@gmail.com>
2024-10-09 20:25:38 +0530

    Used sql instead of dummy data

--- app/(content)/archive/@archive/[[...filter]]/page.js
+++ app/(content)/archive/@archive/[[...filter]]/page.js
@@ -1,56 +1,67 @@
 import NewsList from "@/components/news-List";
-import { getAvailableNewsMonths, getAvailableNewsYears, getNewsForYear, getNewsForYearAndMonth } from "@/lib/news";
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from "@/lib/news";
 import Link from "next/link";
 
-export default async function  FilteredNewsPage({params}){
-    const filter = params.filter;
-    const selectedYear = filter?.[0]
-    const selectedMonth = filter?.[1];
-    
-    let news;
-    let links = await getAvailableNewsYears();
+export default async function FilteredNewsPage({ params }) {
+  const filter = params.filter;
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
 
+  let news;
+  let links = await getAvailableNewsYears();
 
-    if(selectedYear && !selectedMonth){
-        news = await getNewsForYear(selectedYear)
-        links = getAvailableNewsMonths(selectedYear)
-    }
-    if(selectedYear && selectedMonth){
-        news = await getNewsForYearAndMonth(selectedYear, selectedMonth)
-        links = []
+  if (selectedYear && !selectedMonth) {
+    news = await getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+  if (selectedYear && selectedMonth) {
+    news = await getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
 
-    }
+  let newsContent = <p> No news for the selected period.</p>;
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
+  console.log(+selectedYear, typeof +selectedYear)
+  const availableYears = await getAvailableNewsYears()
+  console.log(availableYears);
+  
+  if (selectedYear && !availableYears.includes(+selectedYear)) {
+    throw new Error("Year not Found");
+  }
+  if (
+    selectedMonth &&
+    !getAvailableNewsMonths(selectedYear).includes(+selectedMonth)
+  ) {
+    throw new Error("Invalid Fiter.");
+  }
 
-    let newsContent = <p> No news for the selected period.</p>
-    if(news && news.length >0){
-        newsContent = <NewsList news={ news} />
-    }
-  const abvailableYears =     getAvailableNewsYears()
-    if (
-      (selectedYear && !abvailableYears.includes(+selectedYear)) ||
-      (selectedMonth && !getAailableNewsMonths(+selectedYear).includes(+selectedMonth))
-    ) {
-      throw new Error("Invalid Fiter.");
-    }
+  return (
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
 
-    return (
-      <>
-        <header id="archive-header">
-          <nav>
-            <ul>
-              {links.map((link) => {
-                const href = selectedYear
-                  ? `/archive/${selectedYear}/${link}`
-                  : `/archive/${link}`;
-
-                return (<li key={link}>
-                  <Link href={href} >{link}</Link>
-                </li>)
-              })}
-            </ul>
-          </nav>
-        </header>
-        {newsContent}
-      </>
-    );
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
+  );
 }
--- app/(content)/archive/@latest/default.js
+++ app/(content)/archive/@latest/default.js
@@ -2,7 +2,7 @@ import NewsList from "@/components/news-List";
 import { getLatestNews } from "@/lib/news"
 
 export default async function DefaultNews(){
-    const latestNews = getLatestNews();
+    const latestNews = await getLatestNews();
     return <>
         <h1>Latest News</h1>
         <NewsList news={latestNews}/>
